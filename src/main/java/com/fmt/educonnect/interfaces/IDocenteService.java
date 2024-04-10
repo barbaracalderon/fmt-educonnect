a88f868ef42b167f0a56c9e722f5df12832765d5
barbaracalderon <bcalderoni.ti@gmail.com>
2024-04-10 06:44:17 -0300

    chores: update styling

--- src/main/java/com/fmt/educonnect/interfaces/IDocenteService.java
+++ src/main/java/com/fmt/educonnect/interfaces/IDocenteService.java
@@ -9,12 +9,8 @@ import java.util.List;
 public interface IDocenteService {
 
     DocenteDTO criarDocente(DocenteDTO docenteDTO);
-
     DocenteModel converterParaEntidade(DocenteDTO docenteDTO);
-
     DocenteDTO converterParaResponseDTO(DocenteModel docenteModel);
-
     List<DocenteDTO> listarDocentes();
-
     List<DocenteDTO> converterParaListaDeResponseDTO(List<DocenteModel> docentes);
 }
