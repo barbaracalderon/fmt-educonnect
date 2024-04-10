d8db008120dcc7a636850029593b85a4763133d1
barbaracalderon <bcalderoni.ti@gmail.com>
2024-04-10 04:58:55 -0300

    fix: update DocenteModel with JPA annotations

--- src/main/java/com/fmt/educonnect/models/DocenteModel.java
+++ src/main/java/com/fmt/educonnect/models/DocenteModel.java
@@ -1,13 +1,23 @@
 package com.fmt.educonnect.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "docente")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocenteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
     private LocalDate dataEntrada;
