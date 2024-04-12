21883cd53f1eaa510ebf513152eaff8976ec1850
barbaracalderon <bcalderoni.ti@gmail.com>
2024-04-11 23:15:32 -0300

    fix: readjust AuthenticationController to CadastroController

--- src/main/java/com/fmt/educonnect/controllers/CadastroController.java
+++ src/main/java/com/fmt/educonnect/controllers/CadastroController.java
@@ -2,12 +2,13 @@ package com.fmt.educonnect.controllers;
 
 import com.fmt.educonnect.controllers.dtos.requests.RequestLoginDTO;
 import com.fmt.educonnect.controllers.dtos.responses.ResponseLoginDTO;
-import com.fmt.educonnect.controllers.dtos.requests.RequestRegisterDTO;
+import com.fmt.educonnect.controllers.dtos.requests.RequestCadastroDTO;
 import com.fmt.educonnect.datasource.entities.UserEntity;
 import com.fmt.educonnect.datasource.repositories.UserRepository;
 import com.fmt.educonnect.services.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,38 +19,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("auth")
-public class AuthenticationController {
+@RequestMapping("cadastro")
+public class CadastroController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
-    private TokenService tokenService;
 
-    @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid RequestLoginDTO body) {
-        var usernamePassword = new UsernamePasswordAuthenticationToken(body.login(), body.password());
 
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-        var token = tokenService.generateToken((UserEntity) auth.getPrincipal());
-
-        return ResponseEntity.ok(new ResponseLoginDTO(token));
-
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Valid RequestRegisterDTO body) {
+    @PostMapping()
+    public ResponseEntity<Void> cadastrarUsuario(@RequestBody @Valid RequestCadastroDTO body) {
         if (this.userRepository.findByLogin(body.login()) != null) return ResponseEntity.badRequest().build();
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(body.password());
         UserEntity newUser = new UserEntity(body.login(), encryptedPassword, body.role());
 
         this.userRepository.save(newUser);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
 
---
+++ src/main/java/com/fmt/educonnect/controllers/LoginController.java
@@ -0,0 +1,4 @@
+package com.fmt.educonnect.controllers;
+
+public class LoginController {
+}
