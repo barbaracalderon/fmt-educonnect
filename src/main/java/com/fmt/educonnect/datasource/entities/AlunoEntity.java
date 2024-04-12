423c2285862c5de35b4c125e9ba73909ab94219d
barbaracalderon <bcalderoni.ti@gmail.com>
2024-04-12 17:18:57 -0300

    feat: create AlunoEntity class

---
+++ src/main/java/com/fmt/educonnect/datasource/entities/AlunoEntity.java
@@ -0,0 +1,23 @@
+package com.fmt.educonnect.datasource.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "aluno")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlunoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private LocalDate dataNascimento;
+    private int idCadastro;
+}
