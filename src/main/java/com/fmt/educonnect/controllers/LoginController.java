8f765f0041bf5a245058f8e42bd30b93dca8ee82
barbaracalderon <bcalderoni.ti@gmail.com>
2024-04-15 18:29:59 -0300

    feat: add Logs to LoginController

--- src/main/java/com/fmt/educonnect/controllers/LoginController.java
+++ src/main/java/com/fmt/educonnect/controllers/LoginController.java
@@ -5,6 +5,7 @@ import com.fmt.educonnect.controllers.dtos.responses.ResponseLoginDTO;
 import com.fmt.educonnect.datasource.entities.CadastroEntity;
 import com.fmt.educonnect.services.TokenService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 @RequestMapping("login")
 public class LoginController {
@@ -29,13 +30,14 @@ public class LoginController {
 
     @PostMapping()
     public ResponseEntity login(@RequestBody @Valid RequestLoginDTO body) {
-
         try{
+            log.info("POST /login ---> Chamada para o método.");
             var usernamePassword = new UsernamePasswordAuthenticationToken(body.login(), body.password());
             var auth = this.authenticationManager.authenticate(usernamePassword);
             var token = tokenService.generateToken((CadastroEntity) auth.getPrincipal());
             return ResponseEntity.ok(new ResponseLoginDTO(token));
         } catch (AuthenticationException e) {
+            log.error("STATUS 401 ---> Acesso não autorizado ---> {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login ou senha inválidos.");
         }
 
