23353b3e565386af88010fa7915f49e68ebff310
barbaracalderon <bcalderoni.ti@gmail.com>
2024-04-15 18:45:27 -0300

    feat: add Logs to TurmaController

--- src/main/java/com/fmt/educonnect/controllers/TurmaController.java
+++ src/main/java/com/fmt/educonnect/controllers/TurmaController.java
@@ -6,6 +6,7 @@ import com.fmt.educonnect.infra.exceptions.CursoNotFoundException;
 import com.fmt.educonnect.infra.exceptions.TurmaNotFoundException;
 import com.fmt.educonnect.services.TurmaService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("turmas")
 public class TurmaController {
@@ -28,19 +30,24 @@ public class TurmaController {
     public ResponseEntity<?> criarTurma(@RequestBody @Valid RequestTurmaDTO requestTurmaDTO) {
 
         try {
+            log.info("POST /turmas ---> Chamada para o método.");
             ResponseTurmaDTO responseTurmaDTO = turmaService.criarTurma(requestTurmaDTO);
+            log.info("POST /turmas ---> Sucesso.");
             return ResponseEntity.status(HttpStatus.CREATED).body(responseTurmaDTO);
         } catch (CursoNotFoundException e) {
+            log.error("STATUS 404 ---> Recurso não encontrado ---> {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 
     @GetMapping()
     public ResponseEntity<List<ResponseTurmaDTO>> listarTurmas() {
+        log.info("GET /turmas ---> Chamada para o método.");
         List<ResponseTurmaDTO> ResponseTurmaDTOsList = turmaService.listarTurmas();
         if (ResponseTurmaDTOsList.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         } else {
+            log.info("GET /turmas ---> Sucesso.");
             return ResponseEntity.ok().body(ResponseTurmaDTOsList);
         }
     }
@@ -48,9 +55,12 @@ public class TurmaController {
     @GetMapping("/{id}")
     public ResponseEntity<?> buscarTurmaPorId(@PathVariable("id") Long id) {
         try {
+            log.info("GET /turmas/{} ---> Chamada para o método.", id);
             ResponseTurmaDTO responseTurmaDTO = turmaService.buscarTurmaPorId(id);
+            log.info("GET /turmas/{} ---> Sucesso.", id);
             return ResponseEntity.status(HttpStatus.OK).body(responseTurmaDTO);
         } catch (TurmaNotFoundException e) {
+            log.error("STATUS 404 ---> Recurso não encontrado ---> {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
@@ -58,9 +68,12 @@ public class TurmaController {
     @PutMapping("/{id}")
     public ResponseEntity<?> atualizarTurma(@PathVariable("id") Long id, @RequestBody RequestTurmaDTO requestTurmaDTO) {
         try {
+            log.info("PUT /turmas/{} ---> Chamada para o método.", id);
             ResponseTurmaDTO responseTurmaDTO = turmaService.atualizarTurma(id, requestTurmaDTO);
+            log.info("PUT /turmas/{} ---> Sucesso.", id);
             return ResponseEntity.status(HttpStatus.OK).body(responseTurmaDTO);
         } catch (TurmaNotFoundException e) {
+            log.error("STATUS 404 ---> Recurso não encontrado ---> {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
@@ -68,9 +81,12 @@ public class TurmaController {
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletarTurma(@PathVariable("id") Long id) {
         try {
+            log.info("DELETE /turmas/{} ---> Chamada para o método.", id);
             turmaService.deletarTurma(id);
+            log.info("DELETE /turmas/{} ---> Sucesso.", id);
             return ResponseEntity.noContent().build();
         } catch (TurmaNotFoundException e) {
+            log.error("STATUS 404 ---> Recurso não encontrado ---> {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
